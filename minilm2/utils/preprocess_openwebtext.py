622a7faf76ecd45c5596c4b887a9e0249c833b34
kyv001 <kyvyang@foxmail.com>
2025-06-21 00:07:18 +0800

    多进程进行数据处理

--- minilm2/utils/preprocess_openwebtext.py
+++ minilm2/utils/preprocess_openwebtext.py
@@ -1,27 +1,35 @@
-from . import config
 from transformers import AutoTokenizer, PreTrainedTokenizer  # type: ignore
 import numpy as np
 from tqdm import tqdm
+from multiprocessing import Pool
+import os
+
+def preprocess_text(text: str, out_fname: str, tokenizer: PreTrainedTokenizer):
+    ids = tokenizer.encode(text)
+    seq = np.array(ids, dtype=np.uint16)
+    seq.tofile(out_fname)
 
 def preprocess_openwebtext(text_path: str, bin_path: str, tokenizer: PreTrainedTokenizer):
-    with open(text_path, 'r', encoding='utf-8') as f, open(bin_path, 'wb') as f_bin:
-        n_blank_lines = 0
+    with (  open(text_path, 'r', encoding='utf-8') as f,
+            Pool(16) as pool):
         text = ""
+        fid = 0
         for line in tqdm(f):
-            line = line.strip()
-            if line:
-                text += line + " "
-                continue
-            if not text:
-                continue
-            n_blank_lines += 1
-            if n_blank_lines >= 3:
-                ids = tokenizer.encode(text) + [config.SPECIAL_TOKENS["<eos>"]]
-                np.array(ids, dtype=np.uint16).tofile(f_bin)
-                n_blank_lines = 0
+            if len(line) > 3:
+                text += line
+            if len(text) > 1e6:
+                pool.apply_async(preprocess_text, args=(text, f"{fid}.bin", tokenizer))
+                fid += 1
                 text = ""
-        ids = tokenizer.encode(text) + [config.SPECIAL_TOKENS["<eos>"]]
-        np.array(ids, dtype=np.uint16).tofile(f_bin)
+            
+        pool.close()
+        pool.join()
+    for i in tqdm(range(fid)):
+        with (  open(f"{i}.bin", 'rb') as bin_part,
+                open(bin_path, 'wb') as f_bin):
+            data = bin_part.read()
+            f_bin.write(data)
+            os.remove(f"{i}.bin")
 
 if __name__ == '__main__':
     import sys
