04a659904e339ffd6855d37b5ced2ac9e2d83f6e
kyv001 <kyvyang@foxmail.com>
2025-06-21 17:05:48 +0800

    更新RWKV实现

--- minilm2/llm/model.py
+++ minilm2/llm/model.py
@@ -321,75 +321,10 @@ class TMix(nn.Module):
             decay_low_rank_dim=decay_low_rank_dim,
             a_low_rank_dim=a_low_rank_dim,
             v_low_rank_dim=v_low_rank_dim,
-            gate_low_rank_dim=gate_low_rank_dim
+            gate_low_rank_dim=gate_low_rank_dim,
+            num_hidden_layers=n_blocks,
+            fuse_norm=True
         )
-        with torch.no_grad():  # 参数初始化，从BlinkDL原版实现中复制过来的
-            def ortho_init(x, scale):
-                shape = x.shape
-                if len(shape) == 2:
-                    gain = math.sqrt(shape[0] / shape[1]) if shape[0] > shape[1] else 1
-                    nn.init.orthogonal_(x, gain=gain * scale)
-                elif len(shape) == 3:
-                    gain = math.sqrt(shape[1] / shape[2]) if shape[1] > shape[2] else 1
-                    for i in range(shape[0]):
-                        nn.init.orthogonal_(x[i], gain=gain * scale)
-                else:
-                    assert False
-                return x
-
-            ratio_1_to_0 = 1-(block_id / n_blocks)
-            ratio_0_to_1 = block_id / (n_blocks-1)
-            ddd = torch.ones(dim)
-            for i in range(dim):
-                ddd[i] = i / dim
-
-            x_r = 1.0 -  torch.pow(ddd, 0.2 * ratio_1_to_0)
-            x_w = 1.0 -  torch.pow(ddd, 0.9 * ratio_1_to_0)
-            x_k = 1.0 - (torch.pow(ddd, 0.9 * ratio_1_to_0) + 0.4 * ratio_0_to_1)
-            x_v = 1.0 - (torch.pow(ddd, 0.4 * ratio_1_to_0) + 0.6 * ratio_0_to_1)
-            x_a = 1.0 -  torch.pow(ddd, 0.9 * ratio_1_to_0)
-            x_g = 1.0 -  torch.pow(ddd, 0.2 * ratio_1_to_0)
-            x_x = torch.stack([x_r, x_w, x_k, x_v, x_a, x_g])
-            self.rwkv7.x_x.data.copy_(x_x)
-
-            w1 = torch.zeros(dim, decay_low_rank_dim)
-            w2 = ortho_init(torch.zeros(decay_low_rank_dim, dim), 0.1)
-            decay_speed = torch.ones(dim)
-            for n in range(dim):
-                decay_speed[n] = - 7 + 5 * (n / (dim-1)) ** (0.85+1.0 * ratio_0_to_1 ** 0.5)  # WTF?
-            w0 = decay_speed+0.5
-            self.rwkv7.w_lora.lora[0].weight.data.copy_(w1.T)
-            self.rwkv7.w_lora.lora[2].weight.data.copy_(w2.T)
-            self.rwkv7.w_lora.lora[2].bias.data.copy_(w0)
-
-            a1 = torch.zeros(dim, a_low_rank_dim)
-            a2 = ortho_init(torch.zeros(a_low_rank_dim, dim), 0.1)
-            a0 = torch.zeros(dim)
-            self.rwkv7.a_lora.lora[0].weight.data.copy_(a1.T)
-            self.rwkv7.a_lora.lora[2].weight.data.copy_(a2.T)
-            self.rwkv7.a_lora.lora[2].bias.data.copy_(a0)
-
-            if block_id != 0:  # 第一层没有这个模块
-                v1 = torch.zeros(dim, v_low_rank_dim)
-                v2 = ortho_init(torch.zeros(v_low_rank_dim, dim), 0.1)
-                v0 = torch.zeros(dim)+1.0
-                self.rwkv7.v_lora.lora[0].weight.data.copy_(v1.T)
-                self.rwkv7.v_lora.lora[2].weight.data.copy_(v2.T)
-                self.rwkv7.v_lora.lora[2].bias.data.copy_(v0)
-                del v1, v2, v0
-
-            g1 = torch.zeros(dim, gate_low_rank_dim)
-            g2 = ortho_init(torch.zeros(gate_low_rank_dim, dim), 0.1)
-            self.rwkv7.g_lora.lora[0].weight.data.copy_(g1.T)
-            self.rwkv7.g_lora.lora[2].weight.data.copy_(g2.T)
-
-            self.rwkv7.r_proj.weight.data.uniform_(-0.5 / (dim ** 0.5), 0.5 / (dim ** 0.5))
-            self.rwkv7.k_proj.weight.data.uniform_(-0.05 / (dim ** 0.5), 0.05 / (dim ** 0.5))
-            self.rwkv7.v_proj.weight.data.uniform_(-0.5 / (dim ** 0.5), 0.5 / (dim ** 0.5))
-            self.rwkv7.o_proj.weight.data.zero_()
-
-            del ddd, x_r, x_w, x_k, x_v, x_a, x_g, x_x, w1, w2, w0, a1, a2, a0, g1, g2
-
     def forward(
             self,
             x: torch.Tensor,
